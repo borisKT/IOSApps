91346897f307f4d9af06bf461b948db125d4a19e
Boris Kitt <boristapile@outlook.com>
2020-06-28 23:21:10 +0100

    complete BorisCard

---
+++ BorisCard/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  BorisCard
+//
+//  Created by Boris Kitt on 27/06/2020.
+//  Copyright © 2020 Boris Kitt. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
